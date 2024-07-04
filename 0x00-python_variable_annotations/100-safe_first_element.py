158aae5159c8f5bcf2ae2f8f94cdb99bf12b9a9a
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 23:49:21 +0100

    push

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """"""
+    """world"""
     if lst:
         return lst[0]
     else:
