2986b7acd086742b78ade063f2f9687107085b8e
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 23:28:07 +0100

    push

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """return the right annotation"""
-from typing import Sequence, Tuple, List
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """a list of tuple with sequence and int"""
     return [(i, len(i)) for i in lst]
