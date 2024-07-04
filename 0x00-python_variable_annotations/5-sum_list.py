824aef0dba65aa05424b85fcb2632c33fbde59b1
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 20:37:17 +0100

    push

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,8 @@
 """hello string"""
 
 
+type Vector = list[float]
+
 def sum_list(input_list: list[float]) -> float:
     """with anotaion return the sum of list"""
-    total = 0.0
-    for i in input_list:
-        total += i
-    return total
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+""""""
+def sum_mixed_list(mxd_lst: list[float, int]) -> float:
+    """"""
+    return sum(mxd_lst)
