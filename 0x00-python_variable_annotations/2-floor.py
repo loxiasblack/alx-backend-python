2ae15b1f87cecf882046f94b35d613112cd50b30
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 12:30:25 +0100

    floor anotation

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -6,7 +6,7 @@ retrun floor of a float
 
 def floor(n: float) -> int:
     """with anotation round the float"""
-    if (n - int(n)) <= 0.5:
+    if (n - int(n)) < 0.5:
         return int(n)
     else:
         return int(n) + 1
