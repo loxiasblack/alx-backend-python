a424466ab41174e7b0b6e30d9dccdb701ec4a540
loxiasblack <iliaschokairi@gmail.com>
2024-07-04 13:09:03 +0100

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""return float the sum of the list"""
+
+
+def sum_list(input_list: list) -> float:
+    """with anotaion return the sum of list"""
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
