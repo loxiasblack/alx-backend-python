c704439c254e2dcddfb48a7c5f817ab7d0b30387
loxiasblack <iliaschokairi@gmail.com>
2024-07-08 12:56:43 +0100

    write asynochronus coroutine

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,9 +3,8 @@
 import asyncio
 import random
 
-
 async def wait_random(max_delay=10):
-    """random sleep"""
-    s = random.uniform(0, max_delay)
-    await asyncio.sleep(s)
-    return s
+    """Waits for a random delay between 0 and max_delay seconds and returns the delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
