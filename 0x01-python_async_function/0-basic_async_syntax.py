486a6186632f7e5babed08767cc620c2b47f6667
loxiasblack <iliaschokairi@gmail.com>
2024-07-08 13:16:49 +0100

    push

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-""" The basics of async """
+""" asyncio to return a random sleep """
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """asynchronous coroutine
-
-    Args:
-        max_delay (int): max delay of waiting
-
-    Returns:
-        float: float number
     """
-    actual_delay = random.uniform(0, max_delay)
-    await asyncio.sleep(actual_delay)
-    return actual_delay
+    asynochronous coroutine
+    Args: max_delay (int): max of waiting
+    Returns: float number
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
