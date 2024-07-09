2e82bbee2caea75f49042ae0d6fd2711be87609f
loxiasblack <iliaschokairi@gmail.com>
2024-07-09 11:15:17 +0100

    correct annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-
+"""Asynochronus generator"""
 import random
 import asyncio
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """yield numbers between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
