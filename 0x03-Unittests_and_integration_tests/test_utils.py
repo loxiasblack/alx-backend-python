613e82027c48e7c7c3d81e7a764a88bd695148bf
loxiasblack <iliaschokairi@gmail.com>
2024-07-28 11:47:32 +0100

    push

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 import unittest
 import utils
 from parameterized import parameterized
+from unittest.mock import patch, Mock
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,3 +25,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """test the type of the error raised"""
         self.assertRaises(KeyError)
+
+
+class TestGetJson(unittest.TestCase):
+    """ new class TestGeJson """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload
+        data = utils.get_json(test_url)
+        self.assertEqual(data, test_payload)
+        mock_get.assert_called_once_with(test_url)
